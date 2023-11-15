c2ea3a75db7ceef651383a22bf3e1b63b02e92f6
MarcelGiemza <u80_margie_waw@technichools.com>
2023-11-15 11:44:35 +0100

    first tests update

--- test_main.py
+++ test_main.py
@@ -1,9 +1,11 @@
 from main import ping, app
 
+status_ok = 200
+
 def test_ping():
     assert ping() == "ping"
 
 def test_ping_endpoint():
     client = app.test_client()
     actual = client.get("/ping")
-    assert actual.status_code == 200
+    assert actual.status_code == status_ok
