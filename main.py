1ea36ee75bdf1f6450b01a5e823a6a92d8fdffc9
MarcelGiemza <u80_margie_waw@technichools.com>
2023-11-15 11:01:49 +0100

    basic get users functions

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+users_db = [
+    {"id": 1, "name": "Wojciech", "lastname": "Oczkowski"},
+    {"id": 2, "name": "Marcel", "lastname": "Giemza"}
+            ]
+
 @app.get("/")
 def home():
     return "Hello"
+
+@app.get("/GET/users")
+def get_all_users():
+    return users_db
+
+@app.get("/GET/users/<id>")
+def get_user_by_id(id):
+    return list(filter(lambda user: str(user["id"]) == id, users_db))[0]
 
 if __name__ == "__main__":
     app.run("localhost", 3000)
