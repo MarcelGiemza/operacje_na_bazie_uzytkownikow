a3cec3cbf3a589413f35bea33ad4cd7849bfb3ab
MarcelGiemza <u80_margie_waw@technichools.com>
2023-11-15 11:57:47 +0100

    main changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,20 +7,33 @@ users_db = [
     {"id": 2, "name": "Marcel", "lastname": "Giemza"}
             ]
 
-@app.get("/")
-def home():
-    return "Hello"
-
-@app.get("/GET/users")
-def get_all_users():
-    return users_db
-
-@app.get("/GET/users/<id>")
 def get_user_by_id(id):
     user = list(filter(lambda user: str(user["id"]) == id, users_db))
     if len(user) == 0:
         return {}
     return user[0]
+
+@app.get("/")
+def home():
+    return "Hello"
+
+@app.get("/ping")
+def ping():
+    return "ping"
+
+@app.route("/users/", methods = ["GET", "POST"])
+def users():
+    if request.method == "GET":
+        return users_db
+    if request.method == "POST":
+        pass
+
+@app.route("/users/<id>", methods = ["GET"])
+def users_id(id):
+    if request.method == "GET":
+        return get_user_by_id(id)
+
+
 
 if __name__ == "__main__":
     app.run("localhost", 3000)
